150de53ef8547da83786c4c42a585a69c05cc622
Vasanthakumar M <58352461+vasanthvk-2678@users.noreply.github.com>
2021-01-26 19:37:13 +0530

    Update script.js

--- mod4-sol/script.js
+++ mod4-sol/script.js
@@ -1,9 +1,10 @@
 
+
 (function() {
-    var names = ["vasanth", "hari", "siva", "shishimaroe", "harry", "billy", "buggy", "butcher", "mahn", "shiro"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
-        if (firstLetter === 'h') {
+        if (firstLetter === 'j') {
             byeSpeaker.speak(names[i]);
         } else {
             helloSpeaker.speak(names[i]);
